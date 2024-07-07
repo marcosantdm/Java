ec65c23a5e277b416ef35ea794b43b36a6b74085
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-07-06 23:26:19 -0300

    feat: concatenacao de strings e numericos

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        String nomeCompleto = "Linguagem " + "Java";
+        System.out.println(nomeCompleto);
+        String concatenacao = "?";
+
+        System.out.println(concatenacao);
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+    }
+}
