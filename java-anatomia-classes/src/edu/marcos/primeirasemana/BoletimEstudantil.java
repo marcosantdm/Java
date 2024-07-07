b7d45379972c3ef18db78a86b15e6e4ca84aa52e
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-07-06 22:49:46 -0300

    feat: organização e distribuição das classes/pacotes

--- java-anatomia-classes/src/edu/marcos/primeirasemana/BoletimEstudantil.java
+++ java-anatomia-classes/src/edu/marcos/primeirasemana/BoletimEstudantil.java
@@ -1,3 +1,5 @@
+package edu.marcos.primeirasemana;
+
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 6;
--- java-anatomia-classes/src/edu/marcos/segundasemana/MinhaClasse.java
+++ java-anatomia-classes/src/edu/marcos/segundasemana/MinhaClasse.java
@@ -1,3 +1,5 @@
+package edu.marcos.segundasemana;
+
 public class MinhaClasse {
     public static void main(String[] args) {
         String primeiroNome = "Marcos";
