7c55c0f6db9c6cb74a0fda5a31376708e627511c
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-07-07 12:29:19 -0300

    feat: logica de incremento e decremento

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -15,5 +15,19 @@ public class Operadores {
         System.out.println(concatenacao);
         concatenacao = "1" + (1 + 1 + 1);
         System.out.println(concatenacao);
+
+        // incrementação e decrementação
+        int num = 5;
+
+        for (int i = 0 ; i <= 5 ; i++) {
+            num = num + 2;
+
+            System.out.println(num);
+        }
+
+        System.out.println(++ num);
+        System.out.println(num ++);
+        System.out.println(num --);
+        System.out.println(-- num);
     }
 }
