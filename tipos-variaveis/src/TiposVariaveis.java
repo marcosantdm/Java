fcd40e23a729e5e2df0bbef62d6e8a509610566b
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-07-06 23:13:39 -0300

    feat: declarando constantes que devem ser escritas em caixa alta

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -5,5 +5,13 @@ public class TiposVariaveis {
         short numeroCurto = 1;
         long numeroLongo = numeroCurto;
         short numeroCurto2 = (short) numeroLongo;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; // Constantes devem ser escritas em caixa alta
     }
 }
