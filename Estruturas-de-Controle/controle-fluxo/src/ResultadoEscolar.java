ae145abff06c907b057afc47873530b50bf9ca00
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-07-07 23:43:04 -0300

    feat: operador ternario

--- Estruturas-de-Controle/controle-fluxo/src/ResultadoEscolar.java
+++ Estruturas-de-Controle/controle-fluxo/src/ResultadoEscolar.java
@@ -4,10 +4,14 @@ public class ResultadoEscolar {
 
         if (nota >= 7) {
             System.out.println("Parabéns, você foi aprovado!");
-        } else if (nota >=5 && nota <= 7) {
+        } else if (nota >= 5 && nota <= 7) {
             System.out.println("Você está de recuperação.");
         } else {
             System.out.println("Você foi reprovado.");
         }
+        String resultado = nota >= 7 ? "Voce está de aprovado!" : "Você está de recuperação.";
+
+        System.out.println(resultado);
     }
+
 }
