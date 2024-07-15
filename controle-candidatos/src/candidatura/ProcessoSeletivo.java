6141c025cfcc027f8b195bae321b7390a80473b8
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-07-15 20:11:56 -0300

    feat: analise dos candidatos

---
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,20 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato!");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("Ligar para o candidato com proposta!");
+        } else {
+            System.out.println("Aguardar mais informações dos demais candidatos!");
+        }
+    }
+}
